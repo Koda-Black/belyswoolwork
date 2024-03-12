c418804c5bb50f05125b2857439c44fa24ca0a01
Koda-Black <kodablack.me@gmail.com>
2024-03-12 03:03:12 -0700

    update to data.js file

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -117,33 +117,6 @@ const data = {
         "The Gildan Ultra Cotton T-shirt is made from a substantial 6.0 oz. per sq. yd. fabric constructed from 100% cotton, this classic fit preshrunk jersey knit provides unmatched comfort with each wear. Featuring a taped neck and shoulder, and a seamless double-needle collar, and available in a range of colors, it offers it all in the ultimate head-turning package.",
     },
   ],
-
-  features: [
-    {
-      image: "/images/features/f1.png",
-      title: "Free Shipping",
-    },
-    {
-      image: "/images/features/f2.png",
-      title: "Online Order",
-    },
-    {
-      image: "/images/features/f3.png",
-      title: "Save Money",
-    },
-    {
-      image: "/images/features/f4.png",
-      title: "Promotion",
-    },
-    {
-      image: "/images/features/f5.png",
-      title: "Happy Sell",
-    },
-    {
-      image: "/images/features/f6.png",
-      title: "F24/7 Support",
-    },
-  ],
 };
 
 export default data;
